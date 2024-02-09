fe53b2a195ef9859d6a237a9c53635e494affc13
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-09 18:09:07 +0500

    Refactor getUserById function in user.ts

--- data/user.ts
+++ data/user.ts
@@ -1,27 +1,42 @@
+"use server"
 import { db } from "@/lib/db";
 
-export const getUserByEmail = async (email: string) =>
-{
-    try {
-        const user = await db.user.findUnique({
-            where:{email}
-        })
-        return user;
-    }
-    catch{
-        return null;
-    }
-}
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { email },
+    });
+    return user;
+  } catch {
+    return null;
+  }
+};
 
-export const getUserById = async (id: string | undefined) =>
-{
-    try {
-        const user = await db.user.findUnique({
-            where:{id}
-        })
-        return user;
-    }
-    catch{
-        return null;
-    }
-}
+// export const getUserById = async (id: string | undefined) =>
+// {
+//     try {
+//         const user = await db.user.findUnique({
+//             where:{id}
+//         })
+//         return user;
+//     }
+//     catch{
+//         return null;
+//     }
+// }
+
+export const getUserById = async (
+  id: string | undefined,
+  includeOptions?: any
+) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id },
+      include: includeOptions,
+    });
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user by ID:", error);
+    return null;
+  }
+};
