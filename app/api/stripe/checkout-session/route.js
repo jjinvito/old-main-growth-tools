6da369d0f30a27f3876a267ae00e2e7ccdcf6b47
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-09 18:07:34 +0500

    Add createUserWithStripeCustomerId function and refactor CreateStripeCheckoutSession***

--- app/api/stripe/checkout-session/route.js
+++ app/api/stripe/checkout-session/route.js
@@ -1,66 +1,79 @@
 "use server";
 import { NextResponse } from "next/server";
 import { getUserById } from "@/data/user";
+import { db } from "@/lib/db";
 
 import Stripe from "stripe";
 
-export async function POST(req) {
-  const body = await req.json();
-
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-10-16",
-  });
-
-  const userId = body.userId;
-  const existingUser = await getUserById(userId);
-
-  if (!existingUser) {
-    return NextResponse.json(
-      {
-        error: {
-          code: "no-access",
-          message: "User not found.",
-        },
-      },
-      { status: 401 }
-    );
-  }
-
-  // Create the checkout session
-  const checkoutSession = await stripe.checkout.sessions.create({
-    mode: "subscription",
-    customer: existingUser?.stripeCustomerId,
-    line_items: [
-      {
-        price: body.priceId,
-        quantity: 1,
-      },
-    ],
-    // success_url: `http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}`,
-    success_url:
-      process.env.NEXT_PUBLIC_APP_URL + `?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: process.env.NEXT_PUBLIC_APP_URL,
-    // cancel_url: "http://localhost:3000/cancel",
-    subscription_data: {
-      metadata: {
-        UserId: userId,
-        toolUrl: body.toolUrl,
-      },
-      // trial_period_days: 14,
+const createUserWithStripeCustomerId = async (userEmail, stripeCustomerId) => {
+  await db.user.create({
+    data: {
+      email: userEmail,
+      stripeCustomerId: stripeCustomerId,
     },
   });
+};
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
 
-  if (!checkoutSession.url) {
-    return NextResponse.json(
-      {
-        error: {
-          code: "stripe-error",
-          message: "Could not create checkout session",
+async function CreateStripeCheckoutSession(data) {
+  try {
+    const { userId, emailAddress, priceId, toolUrl, planType } = data;
+    let existingUser;
+
+    if (userId) {
+      existingUser = await getUserById(userId);
+    }
+
+    if (!existingUser && emailAddress) {
+      const stripeCustomer = await stripe.customers.create({
+        email: emailAddress,
+      });
+      await createUserWithStripeCustomerId(emailAddress, stripeCustomer.id);
+      existingUser = { id: null, stripeCustomerId: stripeCustomer.id };
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      customer: existingUser.stripeCustomerId,
+      line_items: [{ price: priceId, quantity: 1 }],
+      discounts: [
+        {
+          coupon: process.env.NEW_USER_CUPON,
+        },
+      ],
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: process.env.NEXT_PUBLIC_APP_URL,
+      subscription_data: {
+        metadata: {
+          UserId: existingUser.id,
+          toolUrl: toolUrl,
+          planType: planType,
         },
       },
-      { status: 500 }
-    );
-  }
+    });
 
-  return NextResponse.json({ session: checkoutSession }, { status: 200 });
+    if (!checkoutSession.url) {
+      throw new Error("Could not create checkout session");
+    }
+
+    return {
+      session: {
+        id: checkoutSession.id,
+        url: checkoutSession.url,
+      },
+    };
+  } catch (error) {
+    console.error("Failed to create Stripe checkout session:", error);
+
+    return {
+      error: {
+        message: "Failed to initiate payment process. Please try again later.",
+        details: error.message,
+      },
+    };
+  }
 }
+
+export { CreateStripeCheckoutSession };
