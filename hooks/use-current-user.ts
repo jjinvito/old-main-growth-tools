4900aba170a5fa8a7de902e3270940ad1adabd51
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-10 18:33:53 +0500

    Remove console.log and add JSON.stringify to use-current-user hook

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -5,6 +5,5 @@ import { useSession } from "next-auth/react";
 export const useCurrentUser = () => {
     const session = useSession();
     JSON.stringify(session) 
-    console.log(session);
     return session?.data?.user;
 }
