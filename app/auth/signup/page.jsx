6b6e7099161947eda408b19050bc0c4d086b8f5b
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-10 15:05:50 +0500

    Add sign up page component

---
+++ app/auth/signup/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SignUpForm } from "@/components/auth/signup-form";
+import Header from "@/components/Header";
+import AuthFooter from "@/components/auth/footer";
+
+const SignInPage = () => {
+  return (
+    <>
+      <Header />
+      <SignUpForm />
+      <AuthFooter />
+    </>
+  );
+};
+
+export default SignInPage;
