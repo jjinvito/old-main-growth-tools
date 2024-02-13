21c54c01a8595091d531cc15eb4328233c0713fb
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-13 08:38:30 +0500

    Add new tool component

---
+++ components/dashboard/add-new-tool.js
@@ -0,0 +1,6 @@
+import SubmitForm from "@/components/SubmitForm";
+import SubmitReview from "@/components/SubmitReview";
+
+export const AddNewTool = () => {
+  return <SubmitForm backbutton={false} />;
+};
