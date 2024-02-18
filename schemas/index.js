4ff0ebb25d54b973a1c5c1e885531a0b17fc7664
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-18 23:16:24 +0500

    Refactor ToolSchema in schemas/index.js

--- schemas/index.js
+++ schemas/index.js
@@ -28,23 +28,37 @@ export const RegisterSchema = z.object({
 
 const priceTypeIdSchema = z.enum(PriceType.map((pt) => pt.id));
 
+
+const itemWordCountValidator = (maxWords) => {
+  return (value) => {
+    if (value.trim().length === 0) {
+      return false;
+    }
+    const wordCount = value.split(/\s+/).filter(Boolean).length;
+    return wordCount <= maxWords;
+  };
+};
+
 export const ToolSchema = z.object({
   name: z.string().min(1, {
     message: "Name is required",
   }),
-  shortDescription: z.string().min(1, {
-    message: "Short Description is required",
-  }),
-  description: z.string().min(1, {
-    message: "Description is required",
-  }),
+  shortDescription: z
+    .string()
+    .min(1, "Short Description is required")
+    .refine(itemWordCountValidator(15), {
+      message: "Short Description must be no more than 15 words",
+    }),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .refine(itemWordCountValidator(150), {
+      message: "Description must be no more than 150 words",
+    }),
   website: z.string().url({
     message: "Valid website URL is required",
   }),
-  // price: z.number().optional(),
-  // pricingType: z.nativeEnum(PriceType),
   pricingType: priceTypeIdSchema,
-  // customPricingInfo: z.string().optional(),
   category: z
     .string()
     .min(1, "Category is required.")
@@ -60,7 +74,38 @@ export const ToolSchema = z.object({
     .refine((val) => TierCategories.map((tier) => tier.id).includes(val), {
       message: "Invalid tier category selected.",
     }),
-  // keyFeatures: z.array(z.string().min(1, { message: "Feature cannot be empty" })).min(1, { message: "At least one key feature is required" }),
-  // useCases: z.array(z.string()).min(1).max(3),
-  // deals: z.string().optional(),
+  keyFeatures: z
+    .array(
+      z
+        .string()
+        .min(1, "Key Feature cannot be empty")
+        .refine(
+          itemWordCountValidator(
+            15,
+            "Each Key Feature must be no more than 15 words"
+          ),
+          {
+            message: "Each Key Feature must be no more than 15 words",
+          }
+        )
+    )
+    .min(3, "At least 3 Key Features are required")
+    .max(6, "No more than 6 Key Features are allowed"),
+  useCases: z
+    .array(
+      z
+        .string()
+        .min(1, "Use Case cannot be empty")
+        .refine(
+          itemWordCountValidator(
+            25,
+            "Each Use Case must be no more than 25 words"
+          ),
+          {
+            message: "Each Use Case must be no more than 25 words",
+          }
+        )
+    )
+    .min(1, "At least 1 Use Case is required")
+    .max(3, "No more than 3 Use Cases are allowed"),
 });
