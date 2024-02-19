82d34f5fd5cf5bd8399fc121726ddad26ec2bc66
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-19 19:34:51 +0500

    Refactor Deals component to add editable states and handle deal changes

--- components/dashboard/formComponents/deals.jsx
+++ components/dashboard/formComponents/deals.jsx
@@ -1,39 +1,152 @@
-import React from 'react';
-const Deals = () => {
-    return (
-        <div>
-            <label className="block text-sm font-semibold mb-1" htmlFor="deals">
-                Deals
-            </label>
+import { useState, React } from "react";
 
-            <div className="flex gap-10">
-                <div className="flex flex-col justify-start gap-1 h-[156px] w-[295px] p-5 border rounded-lg drop-shadow-xl bg-white">
-                    <h1 className="font-bold">LifeTime 50% off</h1>
-                    <p className="text-muted text-sm">
-                        $4.99 <span className="line-through text-dull"> $10</span> / monthly
-                    </p>
-                    <span className="font-medium text-acentGreen p-2 border rounded-lg w-[132px]">
-                        You save $5.01
-                    </span>
-                    <a className="text-DBlue font-medium text-sm" href="#">
-                        Click to learn more
-                    </a>
-                </div>
-                <div className="flex flex-col justify-start gap-1 h-[156px] w-[295px] p-5 border rounded-lg drop-shadow-xl bg-white">
-                    <h1 className="font-bold">-25% off</h1>
-                    <p className="text-muted text-sm">
-                        $4.99 <span className="line-through text-dull"> $10</span> / monthly
-                    </p>
-                    <span className="font-medium text-acentGreen p-2 border rounded-lg w-[132px] text-sm">
-                        You save $3.99
-                    </span>
-                    <a className="text-DBlue font-medium text-sm" href="#">
-                        Click to learn more
-                    </a>
-                </div>
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+import { LuPencil } from "react-icons/lu";
+import { HiOutlineTrash } from "react-icons/hi2";
+import { FaRegCircleCheck } from "react-icons/fa6";
+
+const Deals = ({ fields, register, remove, errors, append, setValue }) => {
+  const [editableStates, setEditableStates] = useState({});
+
+  const toggleEditable = (index) => {
+    setEditableStates((prevEditableStates) => ({
+      ...prevEditableStates,
+      [index]: !prevEditableStates[index],
+    }));
+  };
+
+  const handleAddDeal = () => {
+    append({
+      title: "LifeTime 50% off",
+      price: "$4.99",
+      originalPrice: "$10",
+      validity: "monthly",
+      savings: "You save $5.01",
+      link: "https://enterLinkHere/forAboveButton",
+    });
+  };
+
+  const handleDealChange = (index, propName, value) => {
+    setValue(`deals.${index}.${propName}`, value);
+  };
+
+  return (
+    <div>
+      <label className="block text-sm font-semibold mb-1" htmlFor="deals">
+        Deals
+      </label>
+
+      <div className="flex gap-10">
+        {fields.map((field, index) => (
+          <div
+            key={field.id}
+            className={cn(
+              "flex flex-col justify-start gap-1  h-[156px] w-[295px] p-5 border rounded-lg drop-shadow-xl bg-white relative",
+              editableStates[index] ? "h-[180px]" : ""
+            )}
+          >
+            <input
+              className="font-bold outline-1 w-44 disabled:bg-transparent"
+              defaultValue={field.title}
+              disabled={!editableStates[index]}
+              {...register(`deals.${index}.title`)}
+              onChange={(event) =>
+                handleDealChange(index, "title", event.target.value)
+              }
+            />
+
+            <div className="flex">
+              <input
+                className="text-muted text-sm w-11 outline-1 disabled:bg-transparent"
+                defaultValue={field.price}
+                disabled={!editableStates[index]}
+                onChange={(event) =>
+                  handleDealChange(index, "price", event.target.value)
+                }
+                {...register(`deals.${index}.price`)}
+              />
+              <input
+                className="line-through text-dull w-8 outline-1 disabled:bg-transparent"
+                defaultValue={field.originalPrice}
+                disabled={!editableStates[index]}
+                onChange={(event) =>
+                  handleDealChange(index, "originalPrice", event.target.value)
+                }
+                {...register(`deals.${index}.originalPrice`)}
+              />
+              <input
+                className="text-muted text-sm w-16 outline-1 disabled:bg-transparent"
+                defaultValue={field.validity}
+                disabled={!editableStates[index]}
+                onChange={(event) =>
+                  handleDealChange(index, "validity", event.target.value)
+                }
+                {...register(`deals.${index}.validity`)}
+              />
             </div>
-        </div>
-    );
+
+            <input
+              className="font-medium text-acentGreen p-2 border rounded-lg w-[132px] disabled:bg-transparent"
+              defaultValue={field.savings}
+              disabled={!editableStates[index]}
+              onChange={(event) =>
+                handleDealChange(index, "savings", event.target.value)
+              }
+              {...register(`deals.${index}.savings`)}
+            />
+
+            <a className="text-DBlue font-medium text-sm" href="#">
+              Click to learn more
+            </a>
+            {editableStates[index] && (
+              <input
+                className="text-xs disabled:bg-transparent"
+                defaultValue={field.link}
+                disabled={!editableStates[index]}
+                {...register(`deals.${index}.link`)}
+                onChange={(event) =>
+                  handleDealChange(index, "link", event.target.value)
+                }
+              />
+            )}
+
+            <div className="flex absolute top-5 gap-2 right-3">
+              <button type="button" onClick={() => toggleEditable(index)}>
+                {editableStates[index] ? (
+                  <FaRegCircleCheck className="text-DBlue" />
+                ) : (
+                  <LuPencil className="text-DBlue" />
+                )}{" "}
+              </button>
+              <button
+                type="button"
+                className="h-[20px] w-[20px] text-red-500"
+                onClick={() => remove(index)}
+              >
+                <HiOutlineTrash />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {fields.length < 2 && (
+          <div className="flex flex-col justify-center items-center h-[156px] w-[295px] p-5 border rounded-lg drop-shadow-xl bg-white gap-5">
+            <button type="button" onClick={handleAddDeal}>
+              <Image
+                src="/plusicon.png"
+                width="48"
+                height="48"
+                className="cursor-pointer"
+              />
+            </button>
+            <p className="text-DBlue font-medium">Create new</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Deals;
