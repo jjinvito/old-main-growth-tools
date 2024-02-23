79514dfbe22c1c8f189e9e04a34c28e408fbd1bc
Waleed Amjad <waleedamjad99@hotmail.com>
2024-02-23 23:36:11 +0500

    Add validation and error handling to publishTool function

--- actions/publishTool.js
+++ actions/publishTool.js
@@ -1,5 +1,36 @@
 "use server";
+import { ToolSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
-export const publishTool = (tool) => {
-  console.log("publishTool=", tool);
+export const publishTool = async (toolData, subscriptionId) => {
+  const validatedFields = ToolSchema.safeParse(toolData);
+
+  console.log("Validated fields:", validatedFields.data);
+
+  if (!validatedFields.success) {
+    console.error("Validation failed", validatedFields.error);
+    return {
+      error: "Invalid Fields!",
+      details: validatedFields.error.issues,
+    };
+  }
+  try {
+    const data = validatedFields.data;
+
+    const tool = await db.tool.create({
+      data: {
+        ...data,
+        subscriptionId: subscriptionId,
+        deals: {
+          create: data.deals,
+        },
+      },
+      include: {
+        deals: true,
+      },
+    });
+    return { success: "Tool Updated Success!" };
+  } catch (error) {
+    return { error: "Something went wrong!" };
+  }
 };
