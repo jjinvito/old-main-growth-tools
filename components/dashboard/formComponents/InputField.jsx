d54c538b9a9b64be98dc53bc13aeb5df51e61edd
Waleed Amjad <WaleedAmjad99@hotmail.com>
2024-02-28 15:45:02 +0500

    Update label color in InputField component for darkmode

--- components/dashboard/formComponents/InputField.jsx
+++ components/dashboard/formComponents/InputField.jsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const InputField = ({ id, error, register, name, placeholder }) => (
   <div>
-    <label className={`block text-sm font-semibold mb-1 ${error && "text-red-500"}`} htmlFor={id}>
+    <label className={`block text-sm font-semibold mb-1 dark:text-white ${error && "text-red-500"}`} htmlFor={id}>
       {error ? error.message : placeholder}
     </label>
     <Input id={id} {...register(name)} placeholder={placeholder} />
