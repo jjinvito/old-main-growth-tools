278e464089a9dd817bc3704956c94788181648c9
saudgul <159879663+saudgul5@users.noreply.github.com>
2024-03-19 12:44:04 +0500

    storage  environment set

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['rfbxrkghehiguqabonke.supabase.co'], // Add your Supabase storage URL domain here
+    domains: ['fzdalipvmkykcluwcajm.supabase.co'], // Add your Supabase storage URL domain here
   },
 };
 
