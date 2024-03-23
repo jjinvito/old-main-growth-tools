1608e7a3d4d80cb7b354e2ed7757044a8996594a
saudgul <159879663+saudgul5@users.noreply.github.com>
2024-03-23 16:05:57 +0500

    single tool

--- components/single-tool-page-components/iNewsLetter/screen_shots/ScreenShots.jsx
+++ components/single-tool-page-components/iNewsLetter/screen_shots/ScreenShots.jsx
@@ -14,31 +14,31 @@ export default function ScreenShots({ data }) {
   return (
     <>
       <h1 className=" text-lg font-semibold">Website Screenshots</h1>
-      <div className=" flex flex-col custom-shadow   justify-center items-center min-w-[260px] h-[331px]">
-        <Carousel className=" w-full h-[331px]">
-          <CarouselContent className="p-0">
+      <div className=" flex flex-col p-4  rounded-lg border-2 border-[#F1F1F1]   justify-center items-center min-w-[260px]  ">
+        <Carousel className=" w-10/12   rounded-xl">
+          <CarouselContent className="">
             {data?.screenshots?.map((screenshot, index) => (
-              <CarouselItem key={index} className="p-0 w-full">
-                <Card className=" p-0 w-full ">
-                  <CardContent className=" p-0 w-full">
+              <CarouselItem key={index} className=" w-full p-0">
+                {/* <Card className=" p-0 w-full ">
+                  <CardContent className=" p-0 bg-red-600 w-full"> */}
                     {/* Render each screenshot image */}
                     <Image
                       src={screenshot}
-                      width={1980}
-                      height={720}
+                    width={100}
+                    height={331}
                       alt="screenshot"
-                      className=" w-full  mx-auto  object-cover h-fit "
+                      className=" w-full h-[331px] object-cover  mx-auto    "
                     />
-                  </CardContent>
-                </Card>
+                  {/* </CardContent>
+                </Card> */}
               </CarouselItem>
             ))}
           </CarouselContent>
           <CarouselPrevious className=" lg:block hidden w-fit px-2  " />
-          <CarouselNext className="lg:block hidden  w-fit px-2 " />
+          <CarouselNext className="lg:block hidden  w-fit px-2 cursor-pointer " />
 
-          <CarouselPrevious className=" lg:hidden absolute left-[30%] top-[100%] bottom-0 mt-10 flex  items-center " />
-          <CarouselNext className="lg:hidden absolute right-[30%] top-[100%] bottom-0 mt-10 flex items-center " />
+          <CarouselPrevious className=" lg:hidden cursor-pointer absolute left-[30%] top-[100%] bottom-0 mt-10 flex  items-center " />
+          <CarouselNext className="lg:hidden absolute cursor-pointer right-[30%] top-[100%] bottom-0 mt-10 flex items-center " />
         </Carousel>
       </div>
     </>
